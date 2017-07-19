9465b9c1035ea0a8fab16d427d986b6c981dcced
TRI VI <trivi2710@gmail.com>
2017-07-19 11:07:40 -0400

    minor fix in index.js
    Chore: minor typo fix in index.js

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 var fs = require('fs');
-var express = require('expresss');
-var expressValidator = require('expressValidator');
+var express = require('express');
+var expressValidator = require('express-validator');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var mongoose = require('mongoose');
 
+var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
-app.use(bodyParser.urlencodedParser({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(expressValidator);
 
 // Multer
 var storage =  multer.diskStorage({
 	destination: function(req, file, cb){
 		cb(null, 'public/img/uload');
-	}
+	},
 	filename: function(req, file, cb){
 		cb(null, Date.now() + file.originalname);
 	}
@@ -57,5 +58,21 @@ var Task = mongoose.model('task', taskSchema);
 //Init App
 app.listen(3000, function(){
 	console.log('listening on port 3000');
+});
+
+app.get('/', function (req, res){
+	try {
+		Task.find({}, function(err, data) {
+			if(err){
+				console.log(err);
+				var errorMessage = "Error - Unable to fetch data from database"
+				res.render('mainpage', {error: errorMessage})
+			} else {
+				res.render('mainpage', {data: data});
+			}
+		})
+	} catch(e){
+		res.send(e);
+	}
 });
 
