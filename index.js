2dd3087fc5a1de75c75bcd3f3d62674d6f835ca4
TRI VI <trivi2710@gmail.com>
2017-07-19 10:45:00 -0400

    add mongodb
    Feat: Add mongoldb

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('expresss');
 var expressValidator = require('expressValidator');
 var bodyParser = require('body-parser');
 var multer = require('multer');
+var mongoose = require('mongoose');
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -21,6 +22,40 @@ var storage =  multer.diskStorage({
 });
 var upload = multer({storage: storage}).single('imageFile');
 
+// Mongoose
+mongoose.connect('mongodb://localhost/task_management');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function(){
+	console.log('Connected to Database');
+});
+
+var taskSchema = mongoose.Schema({
+	id: Number,
+	title: String,
+	description: String,
+	image: {
+		data: Buffer,
+		contentType: String
+	},
+	created_at: Date,
+	updated_at: Date
+});
+
+taskSchema.pre('save', function(next){
+	var currentDate = new Date();
+	this.updated_at = currentDate;
+
+	if(!this.createdAt){
+		this.created_at = currentDate;
+	}
+	next();
+});
+
+var Task = mongoose.model('task', taskSchema);
+
+//Init App
 app.listen(3000, function(){
 	console.log('listening on port 3000');
-})
+});
+
